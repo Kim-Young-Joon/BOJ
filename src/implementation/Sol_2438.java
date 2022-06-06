5238fca66e0f6873eda02b52c16d33a19571efa1
Kim-Young-Joon <yj3723@naver.com>
2022-06-06 09:09:43 +0900

    2438 - 별찍기 1

---
+++ src/implementation/Sol_2438.java
@@ -0,0 +1,74 @@
+package implementation;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// 별찍기 1
+public class Sol_2438 {
+    static int N;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+    }
+
+    static void output() {
+        for(int i = 0; i < N; i++) {
+            for(int j = i + 1; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        output();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
