9dcd00cab10cb3522d3efbe32022e931889ada71
Kim-Young-Joon <yj3723@naver.com>
2022-07-13 22:21:22 +0900

    14490 - 백대열

---
+++ src/string_problem/Sol_14490.java
@@ -0,0 +1,25 @@
+package string_problem;
+
+import java.util.Scanner;
+
+// 백대열
+public class Sol_14490 {
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] splited = sc.nextLine().split(":");
+        int N = stoi(splited[0]);
+        int M = stoi(splited[1]);
+        int GCD = gcd(Math.max(N, M), Math.min(N, M));
+
+        System.out.println(N / GCD + ":" + M / GCD);
+    }
+}
