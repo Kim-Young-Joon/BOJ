f97751ccca29a47b83c3603e6919670b66cb41f3
Kim-Young-Joon <yj3723@naver.com>
2022-08-01 19:22:55 +0900

    10799 - 쇠막대기

---
+++ src/stack/Sol_10799.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 쇠막대기
+public class Sol_10799 {
+    static int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if(str.charAt(i-1) == '(') answer += stack.size();
+                else answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
