06a2d2eee61012267cd674df3fe941e7a3e7552a
Kim-Young-Joon <yj3723@naver.com>
2022-08-02 17:44:44 +0900

    2493 - 탑

---
+++ src/stack/Sol_2493.java
@@ -0,0 +1,31 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// 탑
+public class Sol_2493 {
+    public static void main(String[] args) throws Exception {
+        Stack<int[]> st = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer stt = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(stt.nextToken());
+            while (!st.isEmpty()) {
+                if (st.peek()[1] >= v) {
+                    System.out.print(st.peek()[0] + " ");
+                    break;
+                }
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                System.out.print("0 ");
+            }
+            st.push(new int[] { i, v });
+        }
+    }
+}
